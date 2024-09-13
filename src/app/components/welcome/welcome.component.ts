4009ee8770f5caa17743f77d494a205eb3cc0469
Veronika <veronika.hromchenkova@gmail.com>
2024-09-13 13:12:12 +0300

    save welcome photo

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PhotoService } from '../../services/photo.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
@@ -12,11 +12,23 @@ import { AsyncPipe, JsonPipe } from '@angular/common';
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css',
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
   private photoService = inject(PhotoService);
   photoData$!: Observable<any>;
+  private photoSubject = new BehaviorSubject<any>(null);
+  photo$: Observable<any> = this.photoSubject.asObservable();
 
-  constructor() {
-    this.photoData$ = this.photoService.getRandomPhoto({ query: 'house' });
+  constructor() {}
+
+  ngOnInit(): void {
+    const savedPhoto = localStorage.getItem('savedPhoto');
+    if (savedPhoto) {
+      this.photoData$ = of(JSON.parse(savedPhoto));
+    } else {
+      this.photoData$ = this.photoService.getRandomPhoto({ query: 'house' });
+      this.photoData$.subscribe((photo) => {
+        localStorage.setItem('savedPhoto', JSON.stringify(photo));
+      });
+    }
   }
 }
